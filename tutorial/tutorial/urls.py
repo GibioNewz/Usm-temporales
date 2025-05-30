64612d645ee1440dd39cd95bd6557748c44930ce
Nobaittap <83615515+Nobaittap@users.noreply.github.com>
2025-05-30 19:21:51 -0400

    Update urls.py

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('quickstart.urls')),
 ]
